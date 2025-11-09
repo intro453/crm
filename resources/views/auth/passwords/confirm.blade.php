a990ecd8014c2e2f483abca695135fe450b90fc2
intro453 <xctr22@gmail.com>
2025-11-09 22:34:12 +0300

    Restyle auth views with Vuexy template

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,49 +1,47 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+@section('title', __('Confirm Password') . ' - ' . config('app.name'))
 
-                <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
-
-                    <form method="POST" action="{{ route('password.confirm') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Confirm Password') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@section('auth-title')
+    {{ __('Confirm Password') }} 🔐
 @endsection
+
+@section('auth-subtitle')
+    {{ __('Please confirm your password before continuing.') }}
+@endsection
+
+@section('auth-content')
+    <form class="mt-2" method="POST" action="{{ route('password.confirm') }}">
+        @csrf
+        <div class="mb-1">
+            <label class="form-label" for="confirm-password">{{ __('Password') }}</label>
+            <div class="input-group input-group-merge form-password-toggle">
+                <input
+                    class="form-control form-control-merge @error('password') is-invalid @enderror"
+                    id="confirm-password"
+                    type="password"
+                    name="password"
+                    required
+                    autocomplete="current-password"
+                />
+                <span class="input-group-text cursor-pointer"><i data-feather="eye"></i></span>
+            </div>
+            @error('password')
+                <div class="invalid-feedback d-block">{{ $message }}</div>
+            @enderror
+        </div>
+        <button class="btn btn-primary w-100">{{ __('Confirm Password') }}</button>
+    </form>
+@endsection
+
+@section('auth-extra')
+    @if (Route::has('password.request'))
+        <p class="text-center mb-0">
+            <a href="{{ route('password.request') }}"><span>{{ __('Forgot Your Password?') }}</span></a>
+        </p>
+    @endif
+@endsection
+
+@push('page-script')
+    <script src="{{ asset('vuexy/app-assets/js/scripts/pages/auth-login.js') }}"></script>
+@endpush
